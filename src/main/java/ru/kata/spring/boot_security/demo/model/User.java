fcdf9261313cf84de94005b8b14bff5cbf3c7276
Andrey Pechenkin <mcreature@inbox.ru>
2023-03-31 11:50:22 +0300

    User.class метод getAuthorities() исправлен

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -129,7 +129,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getRoleName())).collect(Collectors.toList());
+        return getRoles();
     }
 
     @Override
